3649cc53a5940acb4bfe2e5407bba925922bd6d2
yasminekamal <yassminkamal92@gmail.com>
2022-05-15 07:07:18 -0700

    add books store

---
+++ books.js
@@ -0,0 +1,71 @@
+let books = [{
+        "id": 1,
+        "ISBN": 1,
+        "Name": "book1",
+        "Category": "poetry",
+        "Image": "https://orion-uploads.openroadmedia.com/sm_08a8b96b7e95-514bvhv0ful.jpg",
+        "Pages": 40,
+        "Author": "yasmine",
+        "added": 0,
+        "Price": 6000
+    },
+    {
+        "id": 2,
+        "ISBN": 2,
+        "Name": "book2",
+        "Category": "poetry",
+        "Image": "https://cdn.britannica.com/82/192882-050-79196594/Cover-edition-The-Book-of-Negroes-Lawrence.jpg",
+        "Pages": 50,
+        "Author": "Kamal",
+        "added": 0,
+        "Price": 6000
+    },
+    {
+        "id": 3,
+        "ISBN": 3,
+        "Name": "book3",
+        "Category": "poetry",
+        "Image": "https://www.penguin.co.uk/content/dam/prh/books/308/308972/9780241346891.jpg.transform/PRHDesktopWide_small/image.jpg",
+        "Pages": 200,
+        "Author": "Shymaa",
+        "added": 0,
+
+        "Price": 6000
+    },
+    {
+        "id": 4,
+        "ISBN": 4,
+        "Name": "book4",
+        "Category": "poetry",
+        "Image": "https://img.freepik.com/free-psd/book-table_123684-2.jpg",
+        "Pages": 400,
+        "Author": "yasmine",
+        "added": 0,
+
+        "Price": 6000
+    },
+    {
+        "id": 5,
+        "ISBN": 5,
+        "Name": "book5",
+        "Category": "poetry",
+        "Image": "https://orion-uploads.openroadmedia.com/sm_08a8b96b7e95-514bvhv0ful.jpg",
+        "Pages": 300,
+        "Author": "Mohammed",
+        "added": 0,
+
+        "Price": 6000
+    },
+    {
+        "id": 6,
+        "ISBN": 6,
+        "Name": "book6",
+        "Category": "poetry",
+        "Image": "https://orion-uploads.openroadmedia.com/sm_08a8b96b7e95-514bvhv0ful.jpg",
+        "Pages": 500,
+        "Author": "Mahmoud",
+        "added": 0,
+
+        "Price": 6000
+    },
+];
